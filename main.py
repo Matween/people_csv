dd02298b82f496be6a9f07c9cce850d2aca6d8e1
Matic Bozic <bozicmatic.bm@gmail.com>
2019-08-09 05:41:47 +0200

    Added search option, made output more readable

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ def create_person():
         last_name = input('Enter a last name: ')
     date = create_date()
 
+    print('\n')
     return Person(name, last_name, date)
 
 
@@ -54,7 +55,8 @@ def list_all_people(filename='people.csv', method='r', by='default'):
             if line.rstrip() == 'name;last_name;birthday;':
                 continue
             props = line.split(';')
-            people.append(Person(props[0], props[1], props[2]))
+            date = props[2].split('.')
+            people.append(Person(props[0], props[1], Date(int(date[0]), int(date[1]), int(date[2]))))
     if by == 'first_name':
         people.sort(key=lambda x: x.name)
     elif by == 'last_name':
@@ -63,10 +65,22 @@ def list_all_people(filename='people.csv', method='r', by='default'):
 
 
 def print_people(people):
+    print('\n')
+    if not people:
+        print('There are currently no people. Please add a person first.\n')
+        return
     i = 1
     for person in people:
         print(f'{i}. {person}')
         i += 1
+    print('\n')
+
+
+def search(people, value, field):
+    if field == 'birthday':
+        return filter(lambda x: getattr(x, field).year == int(value), people)
+
+    return filter(lambda x: getattr(x, field) == value, people)
 
 
 if __name__ == '__main__':
@@ -75,12 +89,18 @@ if __name__ == '__main__':
         'B': 'last_name',
     }
 
+    valid_criteria = {
+        '1': 'name',
+        '2': 'last_name',
+        '3': 'birthday',
+    }
+
     while True:
         print('What would you like to do?')
         print('1. Add Person:')
         print('2. List All People:')
         print('3. Find A Person:')
-        print('4. Exit')
+        print('4. Exit\n')
         action = input('Choose numbers between 1 and 4 for your action: ')
         if action == '1':
             add_person(create_person())
@@ -95,7 +115,19 @@ if __name__ == '__main__':
                 print('Printing non sorted list of people.')
                 print_people(list_all_people())
         elif action == '3':
-            break
+            criteria = input('To search for a person, enter:\n'
+                             '1. To search by Name\n'
+                             '2. To search by Last Name\n'
+                             '3. To search by Year of Birth\n'
+                             'Enter criteria: ')
+            if criteria in valid_criteria.keys():
+                people = list_all_people()
+                if len(people) == 0:
+                    print('There are no people to search for. Add a person first.\n')
+                else:
+                    value = input(f'Enter {valid_criteria[criteria]}: ')
+                    results = search(people, value, valid_criteria[criteria])
+                    print_people(results)
         elif action == '4':
             print('Exiting. Bye!')
             break
